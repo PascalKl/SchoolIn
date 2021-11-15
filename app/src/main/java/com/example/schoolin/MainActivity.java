6bec7549841dd9e38a6b20e7fb7f35fe9bcc14e1
PascalKl <pascal.klamrot@gmail.com>
2021-11-15 13:51:56 +0100

    Updated Bottom Navigation

--- app/src/main/java/com/example/schoolin/MainActivity.java
+++ app/src/main/java/com/example/schoolin/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.schoolin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
 
 
@@ -15,5 +20,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.page_home:
+                //Switch to Home page
+                break;
+
+            case R.id.page_search:
+                //Switch to Search page
+                startActivity(new Intent(MainActivity.this, MainActivity_searchpage.class));
+                break;
+            case R.id.page_user:
+                //Switch to User (School) page
+                break;
+        }
+        return false;
     }
 }
