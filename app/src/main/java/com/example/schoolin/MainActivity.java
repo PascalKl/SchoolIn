5c74d6fb9dd5f1cbf35366125431648451e6ef06
PascalKl <pascal.klamroth@gmail.com>
2021-11-15 18:47:21 +0100

    Implementing search page and navigation bar is working now

--- app/src/main/java/com/example/schoolin/MainActivity.java
+++ app/src/main/java/com/example/schoolin/MainActivity.java
@@ -12,31 +12,36 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener;
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
-
+public class MainActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.page_home:
-                //Switch to Home page
-                break;
-
-            case R.id.page_search:
-                //Switch to Search page
-                startActivity(new Intent(MainActivity.this, MainActivity_searchpage.class));
-                break;
-            case R.id.page_user:
-                //Switch to User (School) page
-                break;
-        }
-        return false;
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.page_home:
+                        //navigate to home page (this activity)
+                        //startActivity(new Intent(MainActivity.this, MainActivity.class));
+                        break;
+                    case R.id.page_search:
+                        //navigate to search page
+                        startActivity(new Intent(MainActivity.this, MainActivity_searchpage.class));
+                        break;
+                    case R.id.page_user:
+                        //navigate to user (school) page
+                        //startActivity(new Intent(MainActivity.this, MainActivity_userpage.class));
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
+
+
--- app/src/main/java/com/example/schoolin/MainActivity_searchpage.java
+++ app/src/main/java/com/example/schoolin/MainActivity_searchpage.java
@@ -1,8 +1,13 @@
 package com.example.schoolin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity_searchpage extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class MainActivity_searchpage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_searchpage);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.page_home:
+                        //navigate to home page
+                        startActivity(new Intent(MainActivity_searchpage.this, MainActivity.class));
+                        break;
+                    case R.id.page_search:
+                        //navigate to search page (this activity)
+                        //startActivity(new Intent(MainActivity_searchpage.this, MainActivity_searchpage.class));
+                        break;
+                    case R.id.page_user:
+                        //navigate to user (school) page
+                        //startActivity(new Intent(MainActivity.this, MainActivity_userpage.class));
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
