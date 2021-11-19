4d8dfeeace0f73c8f42f1a2c2526e583abff10d7
PascalKl <pascal.klamrot@gmail.com>
2021-11-19 09:51:04 +0100

    Bugs solved and newSchool.java and activity_new_school.xml created

---
+++ app/src/main/java/com/example/schoolin/newSchool.java
@@ -0,0 +1,14 @@
+package com.example.schoolin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class newSchool extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_school);
+    }
+}
